757a8914903d7199e72475f941dd4830222cba16
David Sousa <david01sousa@Hotmail.com>
2023-07-13 17:33:47 -0300

    finish

---
+++ README.md
@@ -0,0 +1,32 @@
+## como começar:
+```bash
+npm install
+```
+
+## para iniciar localmente: 
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+```
+
+## iniciando o JSON Server:
+
+```bash
+npm run dev:server
+```
+
+## Dependencias usadas:
+
+react
+typescript
+json-server
+react-hook-form
+radix-ui
+axios
+phosphor-react
+styled-components
+zod
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,12 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog';
-import logoImg from '../../assets/logo.svg'
 import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header(){
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoImg} alt="" />
+        <h1>$.Finance</h1>
         <Dialog.Root>
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
